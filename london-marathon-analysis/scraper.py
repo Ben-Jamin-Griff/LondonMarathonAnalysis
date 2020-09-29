69ae8548d324fbaf5bf9fd0a23db9a0ed5c7d99e
Benjamin Griffiths <drjamin1990@gmail.com>
2020-09-29 09:12:39 +0100

    scraper able to run through all 2019 data

--- london-marathon-analysis/scraper.py
+++ london-marathon-analysis/scraper.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
 
-res = requests.get('https://results.virginmoneylondonmarathon.com/2019/?page=1&event=MAS&pid=search')
-soup = BeautifulSoup(res.text, 'html.parser')
-entries = soup.select('li.list-group-item.row')
-
+# Functions
 def createPageData(entries):
     pageData = []
     for idx, i in enumerate(entries):
@@ -16,16 +15,55 @@ def createPageData(entries):
         pageData.append(entry)
     return pageData
 
-pageData = createPageData(entries)
+def cleanData(pageData):
+    for idx, i in enumerate(pageData):
+        row = pageData[idx]
+        for idxx, j in enumerate(row):
+            if idxx == 3:
+                row[idxx] = row[idxx][4:]
+            elif idxx == 4:
+                row[idxx] = row[idxx][13:]
+            elif idxx == 5:
+                row[idxx] = row[idxx][8:]
+            elif idxx == 6:
+                row[idxx] = row[idxx][4:]
+            elif idxx == 7:
+                row[idxx] = row[idxx][6:]
+        pageData[idx] = row
+    return pageData
 
-print(pageData[1])
+# Collecting the 2019 data
+page = 1
+flag = 0
+while flag == 0:
+    try:
+        if page == 1:
+            res = requests.get('https://results.virginmoneylondonmarathon.com/2019/?page=' + str(page) + '&event=MAS&pid=search')
+            soup = BeautifulSoup(res.text, 'html.parser')
+            entries = soup.select('li.list-group-item.row')
+            pageData = createPageData(entries)
+            data = pd.DataFrame(data = None, columns = pageData[0][:3] + pageData[0][4:-1])
+            pageData = pageData[1:]
+            pageData = cleanData(pageData)
+            for idx, i in enumerate(pageData):
+                rowData = pageData[idx][:-1]
+                a_series = pd.Series(rowData, index = data.columns)
+                data = data.append(a_series, ignore_index=True)
+            page += 1
+        else:
+            res = requests.get('https://results.virginmoneylondonmarathon.com/2019/?page=' + str(page) + '&event=MAS&pid=search')
+            soup = BeautifulSoup(res.text, 'html.parser')
+            entries = soup.select('li.list-group-item.row')
+            pageData = createPageData(entries)
+            pageData = pageData[1:]
+            pageData = cleanData(pageData)
+            for idx, i in enumerate(pageData):
+                rowData = pageData[idx][:-1]
+                a_series = pd.Series(rowData, index = data.columns)
+                data = data.append(a_series, ignore_index=True)
+            page += 1
+    except ValueError:
+        print("Oops! No values on this page...")
+        flag = 1
 
-#entries = soup.select('li.list-group-item.row')
-#entry1 = entries[0].select('div.list-field')
-#entry2 = entries[1].select('div.list-field')
-#entry3 = entries[2].select('div.list-field')
-
-#print(f'length of entries {len(entries)}')
-#print(f'length of divs in entry 1 {len(entry1)}')
-#print(f'length of divs in entry 2 {len(entry2)}')
-#print(f'length of divs in entry 2 {len(entry3)}')
+data.to_csv('./data/results2019.csv', index=False)
